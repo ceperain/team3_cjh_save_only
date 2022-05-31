f9973be367bb469197728904e9a152d058dbb6c2
routee90 <routee90@gmail.com>
2022-05-31 16:10:00 +0900

    team3_20220531  161000

---
+++ src/main/java/dev/mvc/team3/HomeCont.java
@@ -0,0 +1,31 @@
+package dev.mvc.team3;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeCont {
+    public HomeCont() {
+        System.out.println("-> HomeCont created.");
+    }
+
+    // http://localhost:9091
+    @RequestMapping(value = { "/", "/index.do" }, method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/index"); // /WEB-INF/views/index.jsp
+
+        return mav;
+    }
+    
+//    @RequestMapping(value = {"/", "/index.do"}, method = RequestMethod.GET)
+//    public String home(Locale locale, Model model) {
+//
+//        return "index";  // /resort/index.jsp
+//    }
+}
