ba91e03081c6389db4149857de0705bb12386c1a
routee90 <routee90@gmail.com>
2022-06-08 18:13:08 +0900

    ljh_데이터정리

--- src/main/java/dev/mvc/store/StoreCont.java
+++ src/main/java/dev/mvc/store/StoreCont.java
@@ -43,7 +43,6 @@ public class StoreCont {
         return mav; // forward
     }
     
-    
     @RequestMapping(value = "/store/list.do", method = RequestMethod.GET)
     public ModelAndView list() {
         ModelAndView mav = new ModelAndView();
